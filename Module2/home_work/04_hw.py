0ebaf42c2426d0abaa981533bb1d75980158d4bf
MarinaDrenina <87034452+MarinaDrenina@users.noreply.github.com>
2021-07-06 23:11:23 +0300

    Update 04_hw.py

--- Module2/home_work/04_hw.py
+++ Module2/home_work/04_hw.py
@@ -14,3 +14,22 @@
 #    #
 #    #
 ######
+
+a = int(input("Сторона квадрата: "))
+i = 2
+line_up = "#"
+while i <= a:
+    line_up = line_up + "#"
+    i = i + 1
+print(line_up)
+i = 2
+line_in = "#"
+while i < a:
+    line_in = line_in + " "
+    i = i + 1
+line_in = line_in + "#"
+i = 2
+while i < a:
+    print(line_in)
+    i = i + 1
+print(line_up)
