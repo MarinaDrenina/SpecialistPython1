c9c077ece2ab9c8abd298a4d64fbb7e0cfa8a2a7
MarinaDrenina <87034452+MarinaDrenina@users.noreply.github.com>
2021-07-08 22:11:16 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,10 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+itog=[]
+numbers= [2, -5, 8, 9, -25, 25, 4]
+for el in numbers:
+    if el>0 and el**0.5%1==0:
+        itog.append(int(el**0.5))
+print(itog)
