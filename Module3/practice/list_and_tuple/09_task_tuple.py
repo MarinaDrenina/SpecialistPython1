56b71dd5cc023cdc113c821a8be629fafbc1fe0e
MarinaDrenina <87034452+MarinaDrenina@users.noreply.github.com>
2021-07-08 20:31:56 +0300

    Update 09_task_tuple.py

--- Module3/practice/list_and_tuple/09_task_tuple.py
+++ Module3/practice/list_and_tuple/09_task_tuple.py
@@ -1,4 +1,12 @@
 # Дано 3 кортежа заполненные произвольными целыми числами.
 # Найдите количество элементов, которые встречаются во всех трех кортежах.
 
-# TODO: your code here
+first=1,2,3,5
+second=5,1,8,7
+third=3,5,7,1
+
+num=0
+for el in first:
+    if second.count(el)>=1 and third.count(el)>=1:
+        num=num+1
+print(num)
