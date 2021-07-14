25f31822b2c58c2217fa35746e1c29e123248b8c
MarinaDrenina <87034452+MarinaDrenina@users.noreply.github.com>
2021-07-14 15:25:31 +0300

    Update 02_hw_file.py

--- Module6/home_work/02_hw_file.py
+++ Module6/home_work/02_hw_file.py
@@ -1,5 +1,11 @@
 # Дан файл data/info.txt, в каждой строке которого содержится строка или целое число
 # Найдите сумму всех чисел, пропуская все строки содержащие не числовые значения
 
-with open("data/info.txt", "r") as f:
-    pass
+numbers = []
+with open("files/info.txt", "r") as f:
+    for line in f:
+        if line.strip().isdigit() or (line.strip()[0]=="-" and line.strip()[1:].isdigit()):
+            numbers.append(int(line.strip()))
+
+print(numbers)
+print(sum(numbers))
