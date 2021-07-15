c45ac96bc6a6244a37e95a29baf1329b4413e898
MarinaDrenina <87034452+MarinaDrenina@users.noreply.github.com>
2021-07-15 20:16:36 +0300

    Update 03_task_struct.py

--- Module8/practice/structs/03_task_struct.py
+++ Module8/practice/structs/03_task_struct.py
@@ -4,3 +4,22 @@
 # 1. Количество элементов списка не превышающие 10
 # 2. Сумму всех положительных элементов списка
 # 3. Среднее арифметическое всех четных элементов
+
+import random
+def rand_num(n):
+    numbers = []
+    for i in range(n):
+        numbers.append(random.randint(-20, 20))
+    return numbers
+
+lst=rand_num(10)
+print(lst)
+
+new_lst=[el for el in lst if el<=10]
+print(len(new_lst))
+
+new_lst_pos=[el for el in lst if el>0]
+print(sum(new_lst_pos))
+
+new_lst_even=[el for el in lst if el%2==0]
+print(sum(new_lst_even)/len(new_lst_even))
