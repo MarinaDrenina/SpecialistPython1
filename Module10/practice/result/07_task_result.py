86f8b4ad727eb36895350bebd25b90914789786a
MarinaDrenina <87034452+MarinaDrenina@users.noreply.github.com>
2021-07-19 16:32:15 +0300

    Update 07_task_result.py

--- Module10/practice/result/07_task_result.py
+++ Module10/practice/result/07_task_result.py
@@ -1,1 +1,9 @@
 # Напишите программу генерирующую и записывающую в файле произвольное 20000-значное целое число.
+import random
+
+f=open("files/Big_char.txt","a",encoding="UTF-8")
+for i in range(20000):
+    if i==0:
+        f.write(str(random.randint(1, 9)))
+    else:
+        f.write(str(random.randint(0,9)))
