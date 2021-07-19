c4e6e2a1e51f7ce571350fd0c1e1077cccebf4d8
MarinaDrenina <87034452+MarinaDrenina@users.noreply.github.com>
2021-07-19 16:15:53 +0300

    Update 06_task_result.py

--- Module10/practice/result/06_task_result.py
+++ Module10/practice/result/06_task_result.py
@@ -5,3 +5,27 @@
 # Формат выходных данных:
 # Выведите исходный список в порядке убывания баллов. Если у некоторых участников одинаковые баллы,
 # то их между собой нужно упорядочить в порядке возрастания идентификационного номера
+
+olimp=[
+    {"id": 1562,
+     "score": 100},
+    {"id": 12500,
+     "score": 500},
+    {"id": 3560,
+     "score": 50},
+    {"id": 15,
+     "score": 500},
+]
+count_id=len(olimp)
+new_olimp=[]
+
+for el in range(count_id):
+    min_id=min(olimp,key=lambda el:el["id"])
+    new_olimp.append(min_id)
+    olimp.remove(min_id)
+
+for el in range(count_id):
+    max_score=max(new_olimp,key=lambda el:el["score"])
+    olimp.append(max_score["id"])
+    new_olimp.remove(max_score)
+print(olimp)
