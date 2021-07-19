47aa1f4a7be8eb4d4d93ea74fff5b32a13e2547e
MarinaDrenina <87034452+MarinaDrenina@users.noreply.github.com>
2021-07-19 17:28:13 +0300

    Update 08_task_result.py

--- Module10/practice/result/08_task_result.py
+++ Module10/practice/result/08_task_result.py
@@ -2,3 +2,25 @@
 # Какие цифры встречаются в числе чаще всего?
 # Если несколько цифр встречаются одинаковое максимальное кол-во раз - найдите любые.
 # Является ли данное число(20000-значное) четным?
+
+f=open("files/Big_char.txt","r",encoding="UTF-8")
+
+char_count_list=[]
+for i in range(10):
+    char_count = 0
+    f.seek(0)
+    for char in f.readline():
+        if int(char)==i:
+            char_count=char_count+1
+    char_count_list.append(char_count)
+
+print(char_count_list)
+print(char_count_list.index(max(char_count_list)))
+
+f.seek(0)
+if int(f.readline())%2==0:
+    print("Четное число")
+else:
+    print("Нечетное число")
+
+f.close()
